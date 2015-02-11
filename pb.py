25ebb07b978efe4c669e76ce021180c7b2f4f46f
alex <akmail255@gmail.com>
2015-02-11 22:55:32 +0300

    11-02-2015 part2

--- pb.py
+++ pb.py
@@ -1,5 +1,8 @@
 import psycopg2
+import re
+
 # http://initd.org/psycopg/docs/usage.html - about psql connection
+
 
 class DB:
     def __init__(self, dbname, user, host=''):
@@ -15,28 +18,56 @@ class DB:
         # Create a table if it does not exist:
         if not bool(self.cur.rowcount):
             self.cur.execute("CREATE TABLE clients (id serial PRIMARY KEY, " +
-                "name varchar, phone varchar);")
+                             "name varchar, phone varchar);")
 
     # Show number of records in the phonebook:
     def number_of_records(self):
         self.cur.execute("SELECT * FROM clients")
         return len(self.cur.fetchall())
 
-    # Add record to the phonebook:
-    def add_record(self, name, phone):
-        self.cur.execute("INSERT INTO clients(name, phone) VALUES(%s, %s)",
-                         (name, phone))
-        self.conn.commit()
-
     # Find record in the PB by one of the fields:
     def find_by_field(self, field, name):
-        self.cur.execute("SELECT * FROM clients WHERE "+field+"=%s;",(name,))
+        self.cur.execute("SELECT * FROM clients WHERE "+field+"=%s;", (name, ))
         return(self.cur.fetchall())
 
     # Show all records in the phonebook:
     def find_all(self):
         self.cur.execute("SELECT * FROM clients;")
         return(self.cur.fetchall())
+
+    # Add record to the phonebook:
+    def add_record(self, name, phone):
+        phone = clean_phone(phone)
+        if not phone.isdigit():
+            return "Phone number is not correct. Can`t add this record."
+        self.cur.execute("INSERT INTO clients(name, phone) VALUES(%s, %s)",
+                         (name, phone))
+        self.conn.commit()
+        return "Record was successfully added."
+
+    # Load file with phone records to our phonebook:
+    def load_from_file(self, f_name):
+        try:
+            self.file = open(f_name, "r")
+        except:
+            return "I can`t find this file."
+        text = self.file.readlines()
+        correct_records = []
+
+        for i in text:
+            i = re.sub(' +', ' ', i)  # Remove double spaces.
+            if re.match("[ .A-za-z\-`]+\s*[0-9() \-]{5,15}", i):
+                correct_records.append(i)
+
+        for r in correct_records:
+            name = re.search("([ .A-Za-z\-`]+)", r).groups()[0].strip(" ")
+            phone = clean_phone(re.search("([0-9() \-]{5,15})", r)
+                                .groups()[0].strip(" "))
+            self.cur.execute("INSERT INTO clients(name, phone) VALUES(%s, %s)",
+                             (name, phone))
+        self.conn.commit()
+        self.file.close()
+        return "Loading successfully finished."
 
     # Delete all records from the phonebook:
     def del_all(self):
@@ -50,9 +81,15 @@ class DB:
         self.cur.close()
         self.conn.close()
 
+
 # Convert list to formatted string while printing:
 def conv_to_str(list1):
     return str(list1).strip('[]').replace('), (', '\n').strip('()')
+
+
+# Clean phone number from marking symbols:
+def clean_phone(phone):
+    return re.sub("[() \-\s]", "", phone)
 
 if __name__ == '__main__':
     p_db = DB('pbook', 'alex')
@@ -60,37 +97,48 @@ if __name__ == '__main__':
     flag = True
     while flag:
         print("="*30+"\nWelcome to my phonebook! \n\nWhat do you want to do?")
-        print("s - show list;\na - add a new record; \nn - find by name;")
-        print("p - find by phone;\ne - erase all book")
-        key=input("-->")
+        print("s - show list; \nn - find by name;\np - find by phone;")
+        print("a - add a new record;\nl - load from file;")
+        print("e - erase all book.\n")
+        key = input(" Choose --> ")
 
-        if key=="s":
-            print ("This the hole phonebook:")
-            #print(str(p_db.find_all()).strip('[]').replace('), (', '\n').strip('()'))
+        if key == "s":
+            print("This is the hole phonebook:")
             print(conv_to_str(p_db.find_all()))
 
-        elif key=='n':  
+        elif key == 'n':
             name = input("Give me a name:")
             list_of_fetch_records = p_db.find_by_field('name', name)
-            if len(list_of_fetch_records)==0:
-                print ("\nThere are no such records.\n")
+            if len(list_of_fetch_records) == 0:
+                print("\nThere are no such records.\n")
             else:
                 print(conv_to_str(list_of_fetch_records))
 
-        elif key=='p':  
+        elif key == 'p':
             phone = input("Give me a phone number:")
             list_of_fetch_records = p_db.find_by_field('phone', phone)
-            if len(list_of_fetch_records)==0:
-                print ("\nThere are no such records.\n")
+            if len(list_of_fetch_records) == 0:
+                print("\nThere are no such records.\n")
             else:
                 print(conv_to_str(list_of_fetch_records))
 
+        elif key == 'a':
+            name = input("Give me a name:")
+            phone = input("Give me a phone number:")
+            print(p_db.add_record(name, phone))
 
-        elif key=='e':
+        elif key == 'l':
+            file_name = input("Give a file name (input.txt - default):")
+            if not file_name:
+                file_name = "input.txt"
+            print(p_db.load_from_file(file_name))
+
+        elif key == 'e':
             p_db.del_all()
             print("\nThe phonebook was erased.\n")
-        
-        key=input("If you want to quit, please, press 'q':")
-        if key=='q':
+
+        key = input("\n"+"="*30+"\nPress 'q' to quit." +
+                    "\nPress any other key to continue:")
+        if key == 'q':
             p_db.close_db()
-            flag=False
+            flag = False
--- tests.py
+++ tests.py
@@ -1,24 +1,48 @@
 import unittest
-#import pb
 from pb import DB
+
 
 class TestPhoneBook(unittest.TestCase):
 
     def setUp(self):
         self.p_db = DB('pbook', 'alex')
-        #self.conn = psycopg2.connect(dbname='pbook', user='alex')
-        #self.cur = self.conn.cursor()
 
+    # Tested methods:
+    # check_table(), number_of_records(), add_record()
     def test_check_table(self):
         self.p_db.check_table()
-        self.assertTrue(self.p_db.number_of_records()>=0)
+        number1 = self.p_db.number_of_records()
+        self.assertTrue(number1 >= 0)
+        self.p_db.add_record("Monika", "344 345 3456")
+        self.p_db.add_record("Lisa Simpson", "456-345-2345")
+        self.assertTrue(self.p_db.number_of_records()-number1 == 2)
 
+    # Tested methods:
+    # del_all(), add_record(), find_all(), find_by_field('name', ...),
+    # find_by_field('phone', ...)
+    def test_add_record(self):
+        self.p_db.del_all()
+        self.p_db.add_record("Alex", "3334444")
+        self.assertIn("Alex", str(self.p_db.find_all()))
+        self.assertIn("3334444", str(self.p_db.find_all()))
+        self.p_db.add_record("Tom Rock Jr.", "8 (742) 345-12-11")
+        self.assertIn("87423451211",
+                      str(self.p_db.find_by_field('name', "Tom Rock Jr.")))
+        self.assertEqual("Phone number is not correct. Can`t add this record.",
+                         self.p_db.add_record("Jim", "8234test8324"))
+        self.assertEqual([], self.p_db.find_by_field("phone", "8234test8324"))
 
-
+    # Tested methods:
+    # del_all(), load_from_file()
+    def test_load_from_file(self):
+        self.p_db.del_all()
+        # index1.txt - doesn`t existl; input.txt - exists:
+        self.assertEqual("I can`t find this file.",
+                         self.p_db.load_from_file("index1.txt"))
+        self.assertEqual("Loading successfully finished.",
+                         self.p_db.load_from_file("input.txt"))
 
     def tearDown(self):
-        #self.cur.close()
-        #self.conn.close()
         self.p_db.close_db()
 
 
